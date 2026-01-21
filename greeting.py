9683b0293e167fb33f39af58adab66c522beed15
Melting Reactor <222494136+MeltingReactor@users.noreply.github.com>
2026-01-21 19:00:27 +0000

    Initial commit

---
+++ greeting.py
@@ -0,0 +1,9 @@
+from time import sleep
+print("Hello!")
+print("What is your name?")
+name = input("Enter your name: ")
+print(name+". That's a nice name.")
+print("Well hello "+name+" and welcome to this program")
+print("Enjoy!")
+sleep(2.58)
+exit()
